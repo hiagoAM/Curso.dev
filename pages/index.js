f88a8de41b61728a8696e3a057f72246127d569e
Hiago <haadamosky@gmail.com>
2024-10-01 04:11:10 -0300

    mensagem para valer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home(){
-    return <h1>Teste de versão atualizada, testando sem o build no package</h1>
+    return <h1>Preciso de uma ajudinha</h1>,
+    <p>
+        Pessoal, eu estou trabalhando em um projeto para empresa, e lá atrás consegui subir no vercel, mas foi feito sem o node ou next, só com html bruto rsrs... por favor, gostaria de uma ajudinha para entender um pouco melhor de como funcionou e quais as vantagens ou desvantagens... acessa ai pessoal https://esg-web-one.vercel.app/
+    </p>
 
 }
 
