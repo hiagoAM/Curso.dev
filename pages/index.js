ccced3f0cd2dc1fbe116734c09ad547a37179c7f
Hiago <haadamosky@gmail.com>
2024-10-05 22:39:39 -0300

    adicionando arquivo  e atualizando

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return <h1>Preciso de uma ajudinha</h1>,
-    <p>
+     <p>
         Pessoal, eu estou trabalhando em um projeto para empresa, e lá atrás consegui subir no vercel, mas foi feito sem o node ou next, só com html bruto rsrs... por favor, gostaria de uma ajudinha para entender um pouco melhor de como funcionou e quais as vantagens ou desvantagens... acessa ai pessoal https://esg-web-one.vercel.app/
     </p>
 
