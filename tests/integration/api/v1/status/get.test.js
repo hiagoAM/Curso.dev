c9ea8219e55e03072e64b6adbd1703f85b6f3190
hiagoAM <109056862+hiagoAM@users.noreply.github.com>
2025-10-23 02:50:10 +0000

    Fazendo a implementação e manipulação dos dados do postgres

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,8 +3,14 @@ test("GET to /api/v1/status shoul return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  console.log(responseBody)
   expect(responseBody.update_at).toBeDefined();
 
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+
 });
