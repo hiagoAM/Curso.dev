2d74e6b0d4d81362ade075275ab060c2da37795f
hiagoAM <109056862+hiagoAM@users.noreply.github.com>
2025-10-25 22:36:42 +0000

    testando interaçãoes com o banco de dados

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -9,7 +9,7 @@ test("GET to /api/v1/status shoul return 200", async () => {
   const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parsedUpdateAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("16.");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 
